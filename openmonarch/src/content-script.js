c5636d680e360fee59411700053933e384bb1730
Milo Akerman <akermanmilo@gmail.com>
2023-04-13 15:29:50 -0400

    Added navigation-based image parsing
    Still needs work.

--- openmonarch/src/content-script.js
+++ openmonarch/src/content-script.js
@@ -1,22 +1,38 @@
-function parseImage(node) {
-    chrome.runtime.sendMessage({ type: "captionImage", img: node.src }).then(response => {
-        if(!response.error) {
-            node.setAttribute("alt", response.data);
-            console.log(response.data);
-        }
-    })
-}
-
-window.onload = function() {
-    document.querySelectorAll("img").forEach(image => {
-        if(image.alt != '') return;
-
-        if(image.complete) {
-            parseImage(image);
-        } else {
-            image.onload = function() {
-                parseImage(image);
+/* TODO: Load with focus
+function nextFocusableElement() {
+    //add all elements we want to include in our selection
+    var focusableElements =
+        'a:not([disabled]), button:not([disabled]), input[type=text]:not([disabled]), [tabindex]:not([disabled]):not([tabindex="-1"])';
+    if (document.activeElement && document.activeElement.form) {
+        var focusable = Array.prototype.filter.call(
+            document.activeElement.form.querySelectorAll(focusableElements),
+            function (element) {
+                //check for visibility while always include the current activeElement
+                return (
+                    element.offsetWidth > 0 ||
+                    element.offsetHeight > 0 ||
+                    element === document.activeElement
+                );
             }
+        );
+        var index = focusable.indexOf(document.activeElement);
+        if (index > -1) {
+            var nextElement = focusable[index + 1] || focusable[0];
+            return nextElement;
         }
-    })
-}
+    }
+}*/
+
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+    if (request.type == "request_focused_img") {
+        if(document.activeElement.src) { 
+            sendResponse({ error: false, data: document.activeElement.src });
+            console.log(document.activeElement);
+        }
+        else sendResponse({ error: true, data: "not_image" });
+    }
+    if (request.type == "send_alert") {
+        if(!request.error) alert("Image description: " + request.data)
+        else alert("ERROR: " + request.data);
+    }
+});
--- openmonarch/src/main.js
+++ openmonarch/src/main.js
@@ -1,29 +1,60 @@
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-    auth: "4d20564813565c3014c5448216c6ee153788215a",
+	auth: "4d20564813565c3014c5448216c6ee153788215a",
 });
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        // parse image on background to avoid CORS issues
-        if (request["type"] == "captionImage") {
-            replicate.run(
-                "rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
-                {
-                    input: {
-                        image: request["img"]
-                    }
-                }
-            ).catch((error) => {
-                console.error(error);
-                sendResponse({ error: true, data: error})
-            }).then(output => {
-                console.log(output)
-                sendResponse({ error: false, data: output });
-            });
-        }
+chrome.commands.onCommand.addListener(async (command) => {
+    if(command == "caption-image") {
+		const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        let image = await chrome.tabs.sendMessage(tab.id, { type: "request_focused_img" });
+		
+		if(image.error) {
+			if(image.data == "not_image") chrome.tabs.sendMessage(tab.id, { type: "send_alert", error: true, data: "No image selected!" });
+			else chrome.tabs.sendMessage(tab.id, { type: "send_alert", error: true, data: "Unknown error encountered." });
+			return;
+		}
+        /*if (image.alt && image.alt != '') {
+            chrome.tabs.sendMessage(tab.id, { type: "send_alert", error: true, data: "This image is already captioned!" });
+			return;
+        }*/
 
-        return true;
-    }
-);
+        if (image.complete) {
+			console.log("Image complete, requesting...");
+            requestCaption(image);
+        } else {
+			console.log("Image not cached. Waiting...")
+            image.onload = function () {
+                requestCaption(image);
+            }
+        }
+    } else {
+		console.log("Command: " + command);
+	}
+});
+
+function requestCaption(imageLink) {
+	console.log("Requesting caption for " + imageLink + "...");
+	// parse image on background to avoid CORS issues
+	if (request["type"] == "captionImage") {
+		replicate.run(
+			"rmokady/clip_prefix_caption:9a34a6339872a03f45236f114321fb51fc7aa8269d38ae0ce5334969981e4cd8",
+			{
+				input: {
+					image: imageLink
+				}
+			}
+
+		).catch(async (error) => {
+			console.error(error);
+
+			const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+			chrome.tabs.sendMessage(tab.id, { type: "send_alert", error: false, data: error });
+		}).then(async (output) => {
+			console.log(output)
+
+			const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+			chrome.tabs.sendMessage(tab.id, { type: "send_alert", error: false, data: output });
+		});
+	}
+}
